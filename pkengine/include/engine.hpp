8d8f0179c39f0c2ae48f8de51bd20d7a0017fcea
DeutschlandNotReal <frenchmapping610@gmail.com>
2026-03-23 00:33:32 +0000

    delarped 3

--- pkengine/include/engine.hpp
+++ pkengine/include/engine.hpp
@@ -1,13 +1,32 @@
 #pragma once
 #include "events.hpp"
 #include "mesh.hpp"
+#include <string>
 
-namespace pk { namespace engine {
+namespace pk::engine {
     extern MeshRenderer mesh_renderer;
 
     namespace frame {
         extern EventPort<float> stepped;
         extern EventPort<glm::vec2> resized;
         extern EventPort<int> closed; // pretty sure closing it just gonna stop everything anyways
+        extern float fps;
     }
-}}
+
+    namespace window {
+        extern std::string title;
+        extern glm::vec2 size;
+        extern glm::vec2 position;
+        void close();
+    }
+
+    namespace input {
+        glm::vec2 mouse_xy();
+
+        extern EventPort<glm::vec2> mouse_moved; // moved from last frame, not new position!!
+        extern EventPort<int> key_pressed;
+        extern EventPort<int> key_released;
+        extern EventPort<int> mouse_clicked; // int for these 2 mean nothing cant have void events
+        extern EventPort<int> mouse_released; 
+    }
+}
